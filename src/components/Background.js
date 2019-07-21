a5f5534bc32e8b613bd51006d33c1d4a61965e93
Eason Chiang <ishiu.chiang@gmail.com>
2019-07-21 11:32:28 +0800

    refactor Background component

--- src/components/Background.js
+++ src/components/Background.js
@@ -14,16 +14,10 @@ const NormalGradientBg = styled.div`
   background: linear-gradient(241deg, rgba(225, 230, 230, 1) 0%, rgba(196, 218, 230, 1) 100%);
 `
 
-const AccentedGradientBg = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  z-index: -1;
-  transition: opacity .5s ease-in-out;
+const AccentedGradientBg = styled(NormalGradientBg)`
   background: rgb(146,200,224);
   background: linear-gradient(241deg, rgba(146,200,224,1) 0%, rgba(195,176,214,1) 100%);
+  transition: opacity .5s ease-in-out;
   opacity: ${props => props.isActive ? 100 : 0 };
 `
 
