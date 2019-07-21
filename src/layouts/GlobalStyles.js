79fa7139cc5648f553c914623c914f998a63d54c
Eason Chiang <ishiu.chiang@gmail.com>
2019-07-21 16:01:52 +0800

    prevent GlobalStyles re-render when SEO changed

--- src/layouts/GlobalStyles.js
+++ src/layouts/GlobalStyles.js
@@ -2,16 +2,20 @@ import React from "react"
 import { Normalize } from 'styled-normalize'
 import { createGlobalStyle } from 'styled-components'
 
-const GlobalStyles = createGlobalStyle`
+const Global = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Cormorant+Garamond:400,600&display=swap');
   body {
     font-family: 'Cormorant Garamond', serif;
   }
 `
 
-export default () => (
-  <>
-    <Normalize />
-    <GlobalStyles />
-  </>
-)
+class GlobalStyles extends React.PureComponent {
+  render = () => (
+    <>
+      <Normalize />
+      <Global />
+    </>
+  )
+}
+
+export default GlobalStyles
