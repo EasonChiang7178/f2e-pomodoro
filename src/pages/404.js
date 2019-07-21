380e838d1abb072d22ebdd3a63dea0d7a9fd1bf5
Eason Chiang <ishiu.chiang@gmail.com>
2019-07-21 16:01:20 +0800

    redirect to root timer page if it has unknown path

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,10 @@
-import React from "react"
+import { useEffect } from "react"
+import { navigate } from "gatsby"
 
-import SEO from "../components/seo"
+export default () => {
+  useEffect(() => {
+    navigate('/')
+  }, [])
 
-const NotFoundPage = () => (
-  <React.Fragment>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </React.Fragment>
-)
-
-export default NotFoundPage
+  return null
+}
