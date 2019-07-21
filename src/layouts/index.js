c943b78ae7f1495d5eda6ba9dd3b24789650397d
Eason Chiang <ishiu.chiang@gmail.com>
2019-07-21 09:23:34 +0800

    add styled-components package

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,10 +8,11 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { Normalize } from 'styled-normalize'
 
 import { TodoContextProvider } from "../contexts/TodoContext"
 import Header from "../components/header"
-import "./index.css"
+
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,24 +26,27 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <TodoContextProvider>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </TodoContextProvider>
+    <>
+      <Normalize />
+      <TodoContextProvider>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0,
+          }}
+        >
+          <main>{children}</main>
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
+      </TodoContextProvider>
+    </>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import styled from "styled-components"
 import { Link } from "gatsby"
 
 import TodoConsumer from '../contexts/TodoContext'
@@ -6,13 +7,19 @@ import TodoConsumer from '../contexts/TodoContext'
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const Hello = styled.div`
+  margin: 3rem auto;
+  background: black;
+  color: white;
+`
+
 const IndexPage = () => (
   <TodoConsumer>
     {({ data }) => (
       <React.Fragment>
         <SEO title="Home" />
         <h1>Hi people</h1>
-        <h1>{String(data.menuOpen)}</h1>
+        <Hello>{String(data.menuOpen)}</Hello>
         <p>Welcome to your new Gatsby site.</p>
         <p>Now go build something great.</p>
         <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
